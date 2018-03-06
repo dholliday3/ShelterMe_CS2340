122ae29260138a3a0ff2b33baf11ede693c3b02d
Daniel Holliday <danielholliday4@gmail.com>
2018-03-06 01:06:31 -0500

    removed project

--- ShelterMe2/app/src/main/gen/m4/cs2340/shelterme/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package m4.cs2340.shelterme;
-
-/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
-public final class BuildConfig {
-  public final static boolean DEBUG = Boolean.parseBoolean(null);
-}
